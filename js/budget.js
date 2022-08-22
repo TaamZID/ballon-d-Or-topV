31966130f9b993abbe5ef9c681f816a3ee7cbc17
TaamZID <tamjidalam00@gmail.com>
2022-08-23 04:50:08 +0600

    final commit

--- js/budget.js
+++ js/budget.js
@@ -2,6 +2,10 @@ let currentBudgetTotal = 0;
 document.getElementById("btn-calculate").addEventListener("click", function () {
   const newBudgetAmount = getInputFieldValueById("budget");
   const budgetTotalElement = document.getElementById("expenses");
-  currentBudgetTotal = cnt * parseFloat(newBudgetAmount);
+  if (cnt > 5) {
+    currentBudgetTotal = 5 * parseFloat(newBudgetAmount);
+  } else {
+    currentBudgetTotal = cnt * parseFloat(newBudgetAmount);
+  }
   budgetTotalElement.innerText = currentBudgetTotal;
 });
--- js/selectedFive.js
+++ js/selectedFive.js
@@ -2,14 +2,19 @@ const pList = [];
 let cnt = 0;
 function displayPlayer() {
   const displayName = document.getElementById("display");
-  displayName.innerHTML = "";
 
-  for (let i = 0; i < pList.length; i++) {
-    cnt = pList.length;
-    const name = pList[i].plName;
-    const li = document.createElement("li");
-    li.innerHTML = `<li>${i + 1}. ${name}</li>`;
-    displayName.appendChild(li);
+  cnt = pList.length;
+  if (cnt < 6) {
+    displayName.innerHTML = "";
+    for (let i = 0; i < pList.length; i++) {
+      const name = pList[i].plName;
+
+      const li = document.createElement("li");
+      li.innerHTML = `<li>${i + 1}. ${name}</li>`;
+      displayName.appendChild(li);
+    }
+  } else {
+    return alert("Can't select more than 5 Players!");
   }
 }
 
